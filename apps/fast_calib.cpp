f416706390951e06085781059ee4acc0f8fb55e5
Amaury Wei <amaury.wei@gmail.com>
2020-12-09 23:11:20 +0100

    added dummy project

---
+++ apps/fast_calib.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file 	fast_calib.cpp
+ * @author 	Amaury Wei (amaury.wei@gmail.com)
+ * @brief 	Sample project for development of Serif libraries
+ * @version 	0.1
+ * @date 	2020-09-12
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include <iostream>
+#include <fast_calib_config.h>
+
+int main(int argc, char* argv[]){
+
+	std::cout << argv[0] << " Version " << fast_calib_VERSION_MAJOR << "."
+		<< fast_calib_VERSION_MINOR << "." << fast_calib_VERSION_PATCH << std::endl
+		<< std::endl;
+}
+
